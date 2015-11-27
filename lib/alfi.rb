bfa8423e7118a7efef1ff44e9bf8d53714fb53a8
Andrew Chen <yongjhih@gmail.com>
2015-11-28 00:18:00 +0800

    Avoid NameError

--- lib/alfi.rb
+++ lib/alfi.rb
@@ -6,5 +6,10 @@ require 'net/http'
 require 'uri'
 require 'colorize'
 require 'alfi/providers'
-
-Dir["#{File.dirname(__FILE__)}/alfi/**/*.rb"].each { |file| require file }
+require 'alfi/cli'
+require 'alfi/search'
+require 'alfi/version'
+require 'alfi/providers/base'
+require 'alfi/providers/maven'
+require 'alfi/providers/offline'
+require 'alfi/providers/bintray'
